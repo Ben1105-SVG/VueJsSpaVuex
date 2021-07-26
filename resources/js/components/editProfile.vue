2aa6f199c0452b1c5168c2efcc88b2a6995e57e7
= <bsaqeyan@gmail.com>
2021-07-26 13:15:28 +0400

    syntax correct

--- resources/js/components/editProfile.vue
+++ resources/js/components/editProfile.vue
@@ -1,73 +1,73 @@
 <template>
-                    <div class="row edit_profile">
-                        <form @submit.prevent="UpdateProfile">
-                            <div class="form-row">
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationServerUsername">Username</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroupPrepend3">@</span>
-                                        </div>
-                                        <input type="text" class="form-control " id="validationServerUsername" v-model="get_auth_user.username " name="username" placeholder="Username" aria-describedby="inputGroupPrepend3" >
-                                    </div>
-                                    <div class="form__field" v-show="errors.username">
-                                        <input type="text" disabled :value=" errors.username" class="bg-danger">
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationServerUsername">Email</label>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control " id="validationServerEmail" v-model="get_auth_user.email" name="email" placeholder="Username" aria-describedby="inputGroupPrepend3" >
-                                    </div>
-                                    <div class="form__field" v-show="errors.email">
-                                        <input type="text" disabled :value=" errors.email" class="bg-danger">
-                                    </div>
-                                </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="validationServerUsername">Phone</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text" id="inputGroupPrepend@phone">+374</span>
-                                        </div>
-                                        <input type="text" class="form-control " id="validationServerPhone" v-model="get_auth_user.phone" placeholder="Phone" name="phone"  aria-describedby="inputGroupPrepend3" >
-                                    </div>
-                                    <div class="form__field" v-show="errors.phone">
-                                        <input type="text" disabled :value=" errors.phone" class="bg-danger">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gender" id="exampleRadios2" v-model="get_auth_user.gender" value="Female">
-                                <label class="form-check-label p-l-5" for="exampleRadios2">
-                                    Female
-                                </label>
-                                <input class="form-check-input" type="radio" name="gender" id="exampleRadios1" v-model="get_auth_user.gender" value="Male" checked>
-                                <label class="form-check-label" for="exampleRadios1">
-                                    Male
-                                </label>
-                            </div>
-                            <div class="form-row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="validationServer03">Locality</label>
-                                    <input type="text" class="form-control " id="validationServer03" placeholder="City" v-model="get_auth_user.locality" name="locality">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleFormControlTextarea1">About me</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" v-model="get_auth_user.about" name="about" rows="3"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <div class="input-group mb-3">
-                                    <div class="custom-file">
-                                        <input type="file" class="custom-file-input" name="avatar" @change="changeAvatar($event)" id="inputGroupFile01">
-                                        <label class="custom-file-label" for="inputGroupFile01">Choose image</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="btn  btn_submit_form" type="submit">Save details</button>
-                        </form>
+    <div class="row edit_profile">
+        <form @submit.prevent="UpdateProfile">
+            <div class="form-row">
+                <div class="col-md-4 mb-3">
+                    <label for="validationServerUsername">Username</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroupPrepend3">@</span>
+                        </div>
+                        <input type="text" class="form-control " id="validationServerUsername" v-model="get_auth_user.username " name="username" placeholder="Username" aria-describedby="inputGroupPrepend3" >
                     </div>
+                    <div class="form__field" v-show="errors.username">
+                        <input type="text" disabled :value=" errors.username" class="bg-danger">
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <label for="validationServerUsername">Email</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control " id="validationServerEmail" v-model="get_auth_user.email" name="email" placeholder="Username" aria-describedby="inputGroupPrepend3" >
+                    </div>
+                    <div class="form__field" v-show="errors.email">
+                        <input type="text" disabled :value=" errors.email" class="bg-danger">
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="validationServerUsername">Phone</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroupPrepend@phone">+374</span>
+                        </div>
+                        <input type="text" class="form-control " id="validationServerPhone" v-model="get_auth_user.phone" placeholder="Phone" name="phone"  aria-describedby="inputGroupPrepend3" >
+                    </div>
+                    <div class="form__field" v-show="errors.phone">
+                        <input type="text" disabled :value=" errors.phone" class="bg-danger">
+                    </div>
+                </div>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="exampleRadios2" v-model="get_auth_user.gender" value="Female">
+                <label class="form-check-label p-l-5" for="exampleRadios2">
+                    Female
+                </label>
+                <input class="form-check-input" type="radio" name="gender" id="exampleRadios1" v-model="get_auth_user.gender" value="Male" checked>
+                <label class="form-check-label" for="exampleRadios1">
+                    Male
+                </label>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 mb-3">
+                    <label for="validationServer03">Locality</label>
+                    <input type="text" class="form-control " id="validationServer03" placeholder="City" v-model="get_auth_user.locality" name="locality">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">About me</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" v-model="get_auth_user.about" name="about" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <div class="input-group mb-3">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="avatar" @change="changeAvatar($event)" id="inputGroupFile01">
+                        <label class="custom-file-label" for="inputGroupFile01">Choose image</label>
+                    </div>
+                </div>
+            </div>
+            <button class="btn  btn_submit_form" type="submit">Save details</button>
+        </form>
+    </div>
 </template>
 
 <script>
